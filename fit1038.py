ddc42f41309ce84426e0f6959cd617f86ab3d3c1
Javier Candeira <javier@candeira.com>
2012-04-11 11:44:12 +1000

    now with a form and post example

--- fit1038.py
+++ fit1038.py
@@ -1,16 +1,25 @@
-import webapp2
+import webapp2, cgi
 
 from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    user = users.get_current_user()
-  
-    if user:
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Hello, %s' % user.nickname())
-    else:
-      self.redirect(users.create_login_url(self.request.uri))
+    self.response.out.write("""
+      <html>
+        <body>
+          <form action="/sign" method="post">
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Sign Guestbook"></div>
+          </form>
+        </body>
+      </html>""")
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+class GuestBook(webapp2.RequestHandler):
+  def post(self):
+    self.response.out.write('<html><body>You wrote:<pre>')
+    self.response.out.write(cgi.escape(self.request.get('content')))
+    self.response.out.write('</pre></body></html>')
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/sign', GuestBook)],
                               debug=True)
